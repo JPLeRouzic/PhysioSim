9c11638bc8602de9bd7ef40ba58118d5e705ff07
Jean-Pierre Le Rouzic <jeanpierre.lerouzic@yahoo.no>
2016-07-17 18:59:37 +0200

    Add files via upload

---
+++ ODEsolver/interpreter/Klammer.java
@@ -0,0 +1,14 @@
+package ODEsolver.interpreter;
+
+//-----------------------------------------------------------------------------
+// Written 2002 by Juergen Arndt.
+//
+//
+// File Klammer.java
+// Use  Globale variable der klammerebene
+//-----------------------------------------------------------------------------
+
+class Klammer
+{
+   static int Ebene;
+}
---
+++ ODEsolver/interpreter/Tan.java
@@ -0,0 +1,17 @@
+package ODEsolver.interpreter;
+
+//-----------------------------------------------------------------------------
+// Written 2002 by Juergen Arndt.
+//
+//
+// File Tan.java
+// Use  funktion fuer das ausfuehren des tangens im baum
+//-----------------------------------------------------------------------------
+
+class Tan implements OpFuncPointer
+{
+   public double exec (double a, double b)
+   {
+      return Math.tan(a);
+   }
+}
